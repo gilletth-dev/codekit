8bfe83854d087663aeb2246237e527416ad4a59a
gilletth-dev <gilletthibault23@gmail.com>
2025-12-07 11:43:07 +0100

    base

---
+++ dropdown/assets/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const dropBtn = document.querySelector(".dropdown");
+// const dropMenu = document.querySelector(".dropdown__content");
+// const dropLinks = document.querySelectorAll(".dropdown__el");
+
+dropBtn.addEventListener("mouseover", dropOpen);
+
+function dropOpen(){
+    dropMenu.classList.add("dropdown__content--show")
+
+    // dropBtn.addEventListener("mouseout", function(){
+    //     dropMenu.classList.remove("dropdown__content--show")
+    // })
+}
+
+
+// for (let i = 0; i < dropLinks.length; i++) {
+//     dropLinks[i].addEventListener("click", function(){
+//         console.log("click");
+//         dropMenu.classList.remove("dropdown__content--show");
+//     });
+// }
+
---
+++ slider/assets/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const btnPrev = document.querySelector(".slider__btn--prev");
+const btnNext = document.querySelector(".slider__btn--next");
+
+btnNext.addEventListener("click", nextSlide);
+btnPrev.addEventListener("click", prevSlide);
+
+function nextSlide(){
+    const elShow = document.querySelector(".slider__el--show");
+    const elNext = elShow.nextElementSibling;
+   
+    elShow.classList.remove("slider__el--show");
+    if(elNext){
+        elNext.classList.add("slider__el--show");
+    } else{
+        const elFirst = elShow.parentElement.firstElementChild;
+        elFirst.classList.add("slider__el--show");
+    }
+    
+}
+
+function prevSlide(){
+    const elShow = document.querySelector(".slider__el--show");
+    const elPrev = elShow.previousElementSibling;
+   
+    elShow.classList.remove("slider__el--show");
+    if(elPrev){
+        elPrev.classList.add("slider__el--show");
+    } else{
+        const elLAst = elShow.parentElement.lastElementChild;
+        elLAst.classList.add("slider__el--show");
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if(event.code == "ArrowLeft"){
+        prevSlide();
+    } else if (event.code == "ArrowRight"){
+        nextSlide();
+    }
+});
