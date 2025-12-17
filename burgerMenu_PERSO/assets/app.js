31adf56c5deb46eda00ad53340d248e36f9f73a2
gilletth-dev <gilletthibault23@gmail.com>
2025-12-17 20:01:54 +0100

    Results
    Personnel

---
+++ burgerMenu_PERSO/assets/app.js
@@ -0,0 +1,25 @@
+"use strict"; 
+
+const body = document.querySelector("body");
+const menuToggle = document.querySelector(".menu__toggle");
+const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll("a");
+
+menu.classList.remove("menu--open");
+
+if(menuToggle){
+    //click btn
+    menuToggle.addEventListener("click", menuOpen);
+
+    //click liens
+    for (let i=0; i < menuLinks.length; i++){
+    menuLinks[i].addEventListener("click", menuOpen)
+}
+}
+
+//toggle du menu
+function menuOpen(){
+    menu.classList.toggle("menu--open");
+    document.body.classList.toggle("no-scroll");
+    console.log("menu open");
+}
